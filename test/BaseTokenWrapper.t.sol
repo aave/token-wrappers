3cff9cfbc4c4f0d53ac515edf2a46c5f14db4760
cedephrase <130931230+cedephrase@users.noreply.github.com>
2023-08-30 11:45:37 -0400

    feat: add additional tests

--- test/BaseTokenWrapper.t.sol
+++ test/BaseTokenWrapper.t.sol
@@ -5,6 +5,7 @@ import {Test, console2} from 'forge-std/Test.sol';
 import {IERC20} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
 import {IPool} from 'aave-v3-core/contracts/interfaces/IPool.sol';
 import {IAToken} from 'aave-v3-core/contracts/interfaces/IAToken.sol';
+import {MintableERC20} from 'aave-v3-core/contracts/mocks/tokens/MintableERC20.sol';
 import {BaseTokenWrapper} from '../src/BaseTokenWrapper.sol';
 
 interface IERC2612 {
@@ -65,6 +66,43 @@ abstract contract BaseTokenWrapperTest is Test {
     assertEq(tokenIn.balanceOf(ALICE), 0, 'Unexpected ending tokenIn balance');
     assertEq(
       IAToken(aTokenOut).balanceOf(ALICE),
+      estimateFinalBalance,
+      'Unexpected ending aToken balance'
+    );
+  }
+
+  function testSupplyTokenToOther() public {
+    IERC20 tokenIn = IERC20(tokenWrapper.TOKEN_IN());
+    assertEq(
+      tokenIn.balanceOf(ALICE),
+      0,
+      'Unexpected starting tokenIn balance'
+    );
+    assertEq(
+      IAToken(aTokenOut).balanceOf(ALICE),
+      0,
+      'Unexpected starting aToken balance'
+    );
+    assertEq(
+      IAToken(aTokenOut).balanceOf(BOB),
+      0,
+      'Unexpected starting aToken balance'
+    );
+
+    uint256 dealAmountScaled = DEAL_AMOUNT * 10 ** tokenInDecimals;
+    _dealTokenIn(ALICE, dealAmountScaled);
+    uint256 estimateFinalBalance = tokenWrapper.getTokenOutForTokenIn(
+      dealAmountScaled
+    );
+
+    vm.startPrank(ALICE);
+    tokenIn.approve(address(tokenWrapper), dealAmountScaled);
+    tokenWrapper.supplyToken(dealAmountScaled, BOB, REFERRAL_CODE);
+    vm.stopPrank();
+
+    assertEq(tokenIn.balanceOf(ALICE), 0, 'Unexpected ending tokenIn balance');
+    assertEq(
+      IAToken(aTokenOut).balanceOf(BOB),
       estimateFinalBalance,
       'Unexpected ending aToken balance'
     );
@@ -144,6 +182,23 @@ abstract contract BaseTokenWrapperTest is Test {
     }
   }
 
+  function testRevertSupplyTokenZeroAmount() public {
+    vm.prank(ALICE);
+    vm.expectRevert('INSUFFICIENT_AMOUNT_TO_SUPPLY');
+    tokenWrapper.supplyToken(0, ALICE, REFERRAL_CODE);
+  }
+
+  function testRevertSupplyTokenInsufficientReceived() public {
+    IERC20 tokenIn = IERC20(tokenWrapper.TOKEN_IN());
+    _dealTokenIn(ALICE, 1);
+
+    vm.startPrank(ALICE);
+    tokenIn.approve(address(tokenWrapper), 1);
+    vm.expectRevert('INSUFFICIENT_WRAPPED_TOKEN_RECEIVED');
+    tokenWrapper.supplyToken(1, ALICE, REFERRAL_CODE);
+    vm.stopPrank();
+  }
+
   function testWithdrawToken() public {
     testSupplyToken();
     IERC20 tokenIn = IERC20(tokenWrapper.TOKEN_IN());
@@ -171,6 +226,72 @@ abstract contract BaseTokenWrapperTest is Test {
     );
     assertLe(
       estimateFinalBalance - tokenIn.balanceOf(ALICE),
+      1,
+      'Unexpected ending tokenIn balance'
+    );
+  }
+
+  function testWithdrawTokenMaxValue() public {
+    testSupplyToken();
+    IERC20 tokenIn = IERC20(tokenWrapper.TOKEN_IN());
+
+    uint256 aTokenBalance = IAToken(aTokenOut).balanceOf(ALICE);
+    assertGt(aTokenBalance, 0, 'Unexpected starting aToken balance');
+    assertEq(
+      tokenIn.balanceOf(ALICE),
+      0,
+      'Unexpected starting tokenIn balance'
+    );
+    uint256 estimateFinalBalance = tokenWrapper.getTokenInForTokenOut(
+      aTokenBalance
+    );
+
+    vm.startPrank(ALICE);
+    IAToken(aTokenOut).approve(address(tokenWrapper), type(uint256).max);
+    tokenWrapper.withdrawToken(type(uint256).max, ALICE);
+    vm.stopPrank();
+
+    assertEq(
+      IAToken(aTokenOut).balanceOf(ALICE),
+      0,
+      'Unexpected ending aToken balance'
+    );
+    assertLe(
+      estimateFinalBalance - tokenIn.balanceOf(ALICE),
+      1,
+      'Unexpected ending tokenIn balance'
+    );
+  }
+
+  function testWithdrawTokenToOther() public {
+    testSupplyToken();
+    IERC20 tokenIn = IERC20(tokenWrapper.TOKEN_IN());
+
+    uint256 aTokenBalance = IAToken(aTokenOut).balanceOf(ALICE);
+    assertGt(aTokenBalance, 0, 'Unexpected starting aToken balance');
+    assertEq(
+      tokenIn.balanceOf(ALICE),
+      0,
+      'Unexpected starting tokenIn balance'
+    );
+    assertEq(tokenIn.balanceOf(BOB), 0, 'Unexpected starting tokenIn balance');
+    uint256 estimateFinalBalance = tokenWrapper.getTokenInForTokenOut(
+      aTokenBalance
+    );
+
+    vm.startPrank(ALICE);
+    IAToken(aTokenOut).approve(address(tokenWrapper), aTokenBalance);
+    tokenWrapper.withdrawToken(aTokenBalance, BOB);
+    vm.stopPrank();
+
+    assertEq(
+      IAToken(aTokenOut).balanceOf(ALICE),
+      0,
+      'Unexpected ending aToken balance'
+    );
+    assertEq(tokenIn.balanceOf(ALICE), 0, 'Unexpected ending tokenIn balance');
+    assertLe(
+      estimateFinalBalance - tokenIn.balanceOf(BOB),
       1,
       'Unexpected ending tokenIn balance'
     );
@@ -229,7 +350,49 @@ abstract contract BaseTokenWrapperTest is Test {
     );
   }
 
-  function testRescueTokens() public {}
+  function testRevertWithdrawTokenZeroAmount() public {
+    vm.prank(ALICE);
+    vm.expectRevert('INSUFFICIENT_AMOUNT_TO_WITHDRAW');
+    tokenWrapper.withdrawToken(0, ALICE);
+  }
+
+  function testRescueTokens() public {
+    uint256 dealAmountScaled = DEAL_AMOUNT * 10 ** 18;
+    MintableERC20 MOCK_TOKEN = new MintableERC20('Mock Token', 'MOCK', 18);
+    MOCK_TOKEN.mint(address(tokenWrapper), dealAmountScaled);
+
+    assertEq(
+      MOCK_TOKEN.balanceOf(address(tokenWrapper)),
+      dealAmountScaled,
+      'Unexpected starting tokenWraper token balance'
+    );
+    assertEq(
+      MOCK_TOKEN.balanceOf(OWNER),
+      0,
+      'Unexpected starting owner token balance'
+    );
+
+    vm.prank(OWNER);
+    tokenWrapper.rescueTokens(MOCK_TOKEN);
+
+    assertEq(
+      MOCK_TOKEN.balanceOf(address(tokenWrapper)),
+      0,
+      'Unexpected final tokenWraper token balance'
+    );
+    assertEq(
+      MOCK_TOKEN.balanceOf(OWNER),
+      dealAmountScaled,
+      'Unexpected final owner token balance'
+    );
+  }
+
+  function testRevertRescueTokensNotOwner() public {
+    MintableERC20 MOCK_TOKEN = new MintableERC20('Mock Token', 'MOCK', 18);
+    vm.prank(ALICE);
+    vm.expectRevert('Ownable: caller is not the owner');
+    tokenWrapper.rescueTokens(MOCK_TOKEN);
+  }
 
   function _dealTokenIn(address user, uint256 amount) internal virtual {
     deal(tokenWrapper.TOKEN_IN(), user, amount);
